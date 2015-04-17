d6907faae27b87ffbc122746913f3e162d314f92
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-04-16 22:41:16 -0400

    test

--- app/routes.js
+++ app/routes.js
@@ -43,7 +43,7 @@ module.exports = function(app, passport) {
             successRedirect : '/profile', // redirect to the secure profile section
             failureRedirect : '/login', // redirect back to the signup page if there is an error
             failureFlash : true // allow flash messages*/
-        }));
+        });
 
         // SIGNUP =================================
         // show the signup form
