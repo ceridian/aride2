850b8efff0132a3f0da6e7eec86cb27a76b45397
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-04-16 22:52:36 -0400

    test

--- config/passport.js
+++ config/passport.js
@@ -76,7 +76,7 @@ module.exports = function(passport) {
     },
     function(req, email, password, done) {
 
-        console.log(req.user);
+        console.log(req.body);
         if (email)
             email = email.toLowerCase(); // Use lower-case e-mails to avoid case-sensitive e-mail matching
 
