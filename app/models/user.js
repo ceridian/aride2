888cf3c13f29e1fe68330b47f3b7a4187450aa61
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-04-16 21:34:18 -0400

    mongo test

--- app/models/user.js
+++ app/models/user.js
@@ -4,7 +4,8 @@ var bcrypt   = require('bcrypt-nodejs');
 
 // define the schema for our user model
 var userSchema = mongoose.Schema({
-
+    first            : String,
+    last             : String,
     local            : {
         email        : String,
         password     : String
